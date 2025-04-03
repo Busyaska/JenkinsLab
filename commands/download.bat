1bef77625c3f80e3c3a58b0b2e239ede43a5bcbf
Busyaska <fox666g@gmail.com>
2025-04-03 17:11:51 +0500

    finish job

--- commands/download.bat
+++
@@ -1,5 +0,0 @@
-D:\Programming\python\python.exe -m venv venv
-call venv\Scripts\activate
-venv\Scripts\python.exe -m pip install --upgrade pip
-venv\Scripts\python.exe -m pip install -r requirements.txt > pip_log.txt 2>&1
-venv\Scripts\python.exe download.py > script_log.txt 2>&1
