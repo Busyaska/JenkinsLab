c52077969f1dae8692870ffb0c5f1b32e137a300
Busyaska <fox666g@gmail.com>
2025-04-03 16:01:20 +0500

    chage commands format

--- download.bat
+++
@@ -1,6 +0,0 @@
-D:\Programming\python\python.exe -m venv venv
-call venv\Scripts\activate
-echo Virtual environment activated!
-D:\Programming\python\python.exe -m pip install --upgrade pip
-pip install -r requirements.txt > pip_log.txt 2>&1
-D:\Programming\python\python.exe download.py > script_log.txt 2>&1
