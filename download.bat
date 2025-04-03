fa85243705ac650e96347035b30156b75fae87c9
Busyaska <fox666g@gmail.com>
2025-04-03 15:39:22 +0500

    Update download.bat

--- download.bat
+++ download.bat
@@ -1,6 +1,6 @@
 @echo off
 D:\Programming\python\python.exe -m venv venv
 call venv\Scripts\activate
-pip install --upgrade pip
+D:\Programming\python\python.exe -m pip install --upgrade pip
 pip install -r requirements.txt > pip_log.txt 2>&1
 D:\Programming\python\python.exe download.py > script_log.txt 2>&1
