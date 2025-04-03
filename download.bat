4a1ace95b601cfd748dcfa03fa44380d061087ba
Busyaska <fox666g@gmail.com>
2025-04-03 15:47:40 +0500

    Update download.bat

--- download.bat
+++ download.bat
@@ -1,5 +1,6 @@
 D:\Programming\python\python.exe -m venv venv
-venv\Scripts\activate
+call venv\Scripts\activate
+echo Virtual environment activated!
 D:\Programming\python\python.exe -m pip install --upgrade pip
 pip install -r requirements.txt > pip_log.txt 2>&1
 D:\Programming\python\python.exe download.py > script_log.txt 2>&1
